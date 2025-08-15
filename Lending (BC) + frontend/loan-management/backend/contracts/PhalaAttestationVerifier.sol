f514b26591859fcf0312d82a8e5181c83f76512e
bakiberkay <baki.b.uzel@campus.tu-berlin.de>
2025-08-15 22:58:47 +0300

    adjusted path

--- Lending (BC) + frontend/loan-management/backend/contracts/PhalaAttestationVerifier.sol
+++ Lending (BC) + frontend/loan-management/backend/contracts/PhalaAttestationVerifier.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import "BytesUtils.sol"; // adjust path if needed
+import "./BytesUtils.sol";
 
 interface IAttestation {
     function verifyAndAttestOnChain(
